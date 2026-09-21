970b9647b5613bf7dee01583af2fdd9e5daeb950
krit <krittamethmek@gmail.com>
2026-09-22 00:02:14 +0700

    แก้ add local + form-post

---
+++ local/src/form-post.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function FormPost() {
+    let [postData, setPostData] = React.useState('')
+    const form = React.useRef()
+        
+    const onSubmitForm = (event) => {
+        event.preventDefault()
+        const formData = new FormData(form.current)
+        const formEnt = Object.fromEntries(formData.entries())
+        fetch('/api/form-post', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formEnt)
+        })
+        .then(response => response.text())
+        .then(result => setPostData(result))
+        .catch(err => alert(err))
+    }
+
+    const inputStyle = {
+        margin: '5px 0'
+    }
+
+  return (
+    <div style={{margin:'30px'}}>
+        <form ref={form} onSubmit={onSubmitForm}>
+            <div>ติดต่อเรา</div>
+            <input type="text" name="name" size="43"placeholder="ชื่อ" 
+            style={inputStyle}/><br/>
+            <input type="email" name="email" size="43" placeholder="อีเมล"
+            style={inputStyle}/><br/>
+            <textarea name="message" rows="4" cols="40" placeholder="ข้อความ"
+            style={inputStyle}></textarea><br/>
+            <button>ตกลง</button>
+        </form>
+        <br/>
+        <div dangerouslySetInnerHTML={{__html: postData}}></div>
+    </div>
+  )
+}
